b29c1e4d3e11cc8f3ad932f47dec1cf698f092b6
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-20 01:47:01 -0400

    nav bar and animation complete

--- script.js
+++ script.js
@@ -4,91 +4,78 @@
 // console.log(form)
 // console.log(input)
 
-const url = "https://swapi.co/api/films/"
+const urlFilms = "https://swapi.co/api/films"
 
-fetch(url)
+    fetch(urlFilms)
+            .then(res => {
+                    return res.json()
+            })
+            .then(res => {
+                    console.log(res.results) // display name of Film
+                    
+            })
+            .catch(err => {
+                    console.log("something went wrong...", err)
+        })
+
+        // console.log("bob")
+
+const urlPeople = "https://swapi.co/api/people/" //setting equal to URL that access people
+
+    fetch(urlPeople)
+            .then(res => {
+                    return res.json()
+            })
+            .then(res => {
+                // console.log("this is two")
+                    console.log(res.results) // display name of People
+                    
+            })
+            .catch(err => {
+                    console.log("something went wrong...", err)
+        })
+
+const urlSpecies = "https://swapi.co/api/species/" //setting equal to URL that access people
+
+    fetch(urlSpecies)
+            .then(res => {
+                    return res.json()
+            })
+            .then(res => {
+                // console.log("this is two")
+                    console.log(res.results) // display name of Species
+                    
+            })
+            .catch(err => {
+                    console.log("something went wrong...", err)
+        })
+
+const urlStarships = "https://swapi.co/api/starships/" //setting equal to URL that access people
+
+    fetch(urlStarships)
+            .then(res => {
+                    return res.json()
+            })
+            .then(res => {
+                // console.log("this is two")
+                    console.log(res.results) // display name of Starships
+                    
+            })
+            .catch(err => {
+                    console.log("something went wrong...", err)
+        })
+
+const urlVehicles = "https://swapi.co/api/vehicles/" //setting equal to URL that access people
+
+    fetch(urlVehicles)
         .then(res => {
                 return res.json()
         })
         .then(res => {
-                console.log(res.name) // display name of Pokemon
+            // console.log("this is two")
+                console.log(res.results) // display name of Vehicles
+                
         })
         .catch(err => {
                 console.log("something went wrong...", err)
         })
- 
-
-
-
-// const url2 = "https://swapi.co/api/people/" //setting equal to URL that access berries
-// // const ul = document.querySelector('ul') //create variable for UL on html page
-
-// fetch(url2) //to make request to get all data from specified URL
-// .then(res => res.json()) //convwrting response to JSON
-// .then(res => {
-//     console.log(res.results) //accsessing results with dot notation, got back an object from response 
-//     let berries = res.results 
-//     for (let i = 0; i < berries.length; i++) {
-//         console.log(berries[i]) //console logging 
-//         let li = document.createElement('li') // create new element for each variable we are looping through
-//         li.innerText = berries[i].name
-//         ul.appendChild(li)
-//     }
-// })
-
-
-
-// const url3 = "https://swapi.co/api/species/" //setting equal to URL that access berries
-// // const ul = document.querySelector('ul') //create variable for UL on html page
-
-// fetch(url2) //to make request to get all data from specified URL
-// .then(res => res.json()) //convwrting response to JSON
-// .then(res => {
-//     console.log(res.results) //accsessing results with dot notation, got back an object from response 
-//     let berries = res.results 
-//     for (let i = 0; i < berries.length; i++) {
-//         console.log(berries[i]) //console logging 
-//         let li = document.createElement('li') // create new element for each variable we are looping through
-//         li.innerText = berries[i].name
-//         ul.appendChild(li)
-//     }
-// })
-
-
-
-
-// const url4 = "https://swapi.co/api/starships/" //setting equal to URL that access berries
-// // const ul = document.querySelector('ul') //create variable for UL on html page
-
-// fetch(url2) //to make request to get all data from specified URL
-// .then(res => res.json()) //convwrting response to JSON
-// .then(res => {
-//     console.log(res.results) //accsessing results with dot notation, got back an object from response 
-//     let berries = res.results 
-//     for (let i = 0; i < berries.length; i++) {
-//         console.log(berries[i]) //console logging 
-//         let li = document.createElement('li') // create new element for each variable we are looping through
-//         li.innerText = berries[i].name
-//         ul.appendChild(li)
-//     }
-// })
-
-
-
-
-
-// const url5 = "https://swapi.co/api/vehicles/" //setting equal to URL that access berries
-// // const ul = document.querySelector('ul') //create variable for UL on html page
-
-// fetch(url2) //to make request to get all data from specified URL
-// .then(res => res.json()) //convwrting response to JSON
-// .then(res => {
-//     console.log(res.results) //accsessing results with dot notation, got back an object from response 
-//     let berries = res.results 
-//     for (let i = 0; i < berries.length; i++) {
-//         console.log(berries[i]) //console logging 
-//         let li = document.createElement('li') // create new element for each variable we are looping through
-//         li.innerText = berries[i].name
-//         ul.appendChild(li)
-//     }
-// })
