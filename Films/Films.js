970dada7297a393e1e55c10416137c73136790de
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-10-21 01:06:34 -0400

    People page complete

--- Films/Films.js
+++ Films/Films.js
@@ -2,19 +2,19 @@ const urlFilms = "https://swapi.co/api/films"
 const grabFilmDivTag = document.querySelector('.filmInfo') 
 
     fetch(urlFilms)
-            .then(res => res.json())
-            .then(res => {
-                console.log(res.results)
-                res.results.forEach(indices => {
-                    let newP2 = document.createElement("p")
+            .then(res => res.json()) //this is bringing in the data and converting it to Json data
+            .then(res => {       //this  is using an arrow function to console log the results.
+                console.log(res.results) //results is what is holding the information 
+                res.results.forEach(indices => { //indicies is just the name of the vaiable, but this for each loop is stated here basically stating for each indice
+                    let newP2 = document.createElement("p") // create new paragraph for each indice as it loops through
                     //console.log(newP2)
-                    let newContent = document.createTextNode(indices.title)
+                    let newContent = document.createTextNode(indices.title) // this creates a new variable that is equal to creating a text node, indicies is the array and .title is gonna give me the title of each film
                     //console.log(newContent)
-                    newP2.appendChild(newContent);
+                    newP2.appendChild(newContent); //then in order to add this information I append it.
                     console.log(newP2)
-                    grabFilmDivTag.appendChild(newP2)
+                    grabFilmDivTag.appendChild(newP2) // then i append it to the tag/div i set
                     //console.log(indices.title)
-                    let p3 = document.createElement("p")
+                    let p3 = document.createElement("p") // here I am doing the same thing as above but creating a new paragraph
                     let crawl = document.createTextNode(indices.opening_crawl)
                     p3.appendChild(crawl);
                     grabFilmDivTag.appendChild(p3)
